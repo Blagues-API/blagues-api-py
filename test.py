ad087ba39dea561dd3ab2bac43d5c31070c05187
DraftProducts <nicovanaarsen@gmail.com>
2021-06-13 01:09:38 +0200

    refactor: ✅ Refactor of tests
    Co-authored-by: coco875 <pereira.jannin@gmail.com>

--- test.py
+++ test.py
@@ -1,62 +1,46 @@
 import asyncio
-import unittest
-import os
-import dotenv
 
+import os
+from unittest import TestCase
+from dotenv import dotenv_values
 from blagues_api import BlaguesAPI, BlagueType, Blague
 
-dotenv.load_dotenv()
+config = dotenv_values(".env")
 
-TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzI0OTQxNDcyMzY3NjQwNTk2IiwibGltaXQiOjEwMCwia2V5IjoiOHJ2QVdPaEhzdHJlNFRPMFA0MXNZRlMzZTVuajA3QnJOOER2eUwyVFhvczRWUGZ0a0UiLCJjcmVhdGVkX2F0IjoiMjAyMS0wNi0wOVQxNzoyMzozMiswMDowMCIsImlhdCI6MTYyMzI1OTQxMn0.QyqzFGEigCz0yd0K_HJkk8lIABrpI9UDf3UVfSkDIsc" #os.getenv('MY_ENV_VAR')
+blagues = BlaguesAPI(config.token)
 
-blagues = BlaguesAPI(TOKEN)
-
-class WrapperTests(unittest.TestCase):
+class WrapperTests(TestCase):
 
     def test_token(self):
         """ Check token existance in .env """
-        self.assertIsInstance(TOKEN, str)
-    
+        self.assertIsInstance(config.token, str)
+
     def test_random_joke(self):
         """ Check random joke """
         loop = asyncio.get_event_loop()
-        reponce = loop.run_until_complete(blagues.random())
-        self.assertIsInstance(reponce, Blague)
-        self.assertIsInstance(reponce.id, int)
-        self.assertIn(reponce.type, BlagueType)
-        self.assertIsInstance(reponce.joke, str)
-        self.assertIsInstance(reponce.answer, str)
+        response = loop.run_until_complete(blagues.random())
+        self.assertIsInstance(response, Blague)
 
     def test_random_with_disallowed(self):
         """ Check random with disallowed DARK type """
         loop = asyncio.get_event_loop()
-        reponce = loop.run_until_complete(blagues.random(disallow=["dark"]))
-        self.assertIsInstance(reponce, Blague)
-        self.assertIsInstance(reponce.id, int)
-        self.assertNotEqual(reponce.type, BlagueType.DARK)
-        self.assertIsInstance(reponce.joke, str)
-        self.assertIsInstance(reponce.answer, str)
+        response = loop.run_until_complete(blagues.random(disallow=["dark"]))
+        self.assertIsInstance(response, Blague)
+        self.assertNotEqual(response.type, BlagueType.DARK)
 
     def test_random_with_allowed(self):
         """ Check random with disallowed DARK type """
         loop = asyncio.get_event_loop()
-        reponce = loop.run_until_complete(blagues.random_categorized(disallow=["dark"]))
-        self.assertIsInstance(reponce, Blague)
-        self.assertIsInstance(reponce.id, int)
-        self.assertEqual(reponce.type, BlagueType.DARK)
-        self.assertIsInstance(reponce.joke, str)
-        self.assertIsInstance(reponce.answer, str)
+        response = loop.run_until_complete(blagues.random_categorized(category=BlagueType.DARK))
+        self.assertIsInstance(response, Blague)
+        self.assertEqual(response.type, BlagueType.DARK)
     
     def test_by_id(self):
         """ Check random with disallowed DARK type """
         loop = asyncio.get_event_loop()
-        reponce = loop.run_until_complete(blagues.from_id(1))
-        self.assertIsInstance(reponce, Blague)
-        self.assertEqual(reponce.id, 1)
-        self.assertEqual(reponce.type, BlagueType)
-        self.assertIsInstance(reponce.joke, str)
-        self.assertIsInstance(reponce.answer, str)
-
+        response = loop.run_until_complete(blagues.from_id(1))
+        self.assertIsInstance(response, Blague)
+        self.assertEqual(response.id, 1)
 
 if __name__ == '__main__':
     unittest.main()
