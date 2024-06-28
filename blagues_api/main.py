caa988600d106f0fd5baa2ad4c627fa929967f52
Alexis <awaknocode@gmail.com>
2024-06-28 14:19:30 +0200

    Rafraichissement de la librairie (Le code est vieux)

--- blagues_api/main.py
+++ blagues_api/main.py
@@ -1,10 +1,8 @@
 import aiohttp
 import pydantic
-
 from typing import List
 from enum import Enum
-
-
+import asyncio
 class BlagueType(str, Enum):
     GLOBAL = "global"
     DEV = "dev"
@@ -16,7 +14,6 @@ class BlagueType(str, Enum):
     def __str__(self):
         return str(self.value)
 
-
 class Blague(pydantic.BaseModel):
     id: int
     type: BlagueType
@@ -25,55 +22,45 @@ class Blague(pydantic.BaseModel):
 
 class CountJoke(pydantic.BaseModel):
     count: int
-
+    
 class BlaguesAPI:
     def __init__(self, token: str):
         self.token = token
-
+        self.base_url = "https://www.blagues-api.fr/api"
+        self.headers = {'Authorization': 'Bearer ' + self.token}
+    
+    
+     
+    
+    async def _get(self, url: str, params: dict = None) -> dict:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            async with session.get(self.base_url+url, headers=self.headers, params=params) as resp:
+                
+                return await resp.json()
+            
     async def random(self, *, disallow: List[str] = None) -> Blague:
-        async with aiohttp.ClientSession(raise_for_status=True) as session:
-            
-            url = "https://www.blagues-api.fr/api/random"
+        endpoint = "/random"
+        params = {"disallow": disallow} if disallow else {}
+        data = await self._get(endpoint, params)
+        
+        return Blague.parse_obj(data)
 
-            if disallow is not None:
-                params = {"disallow": disallow}
-            else:
-                params = {}
-
-            headers = {'Authorization': 'Bearer ' + self.token}
-
-            async with session.get(url, params=params, headers=headers) as resp:
-                data = await resp.json()
-                return Blague.parse_obj(data)
-    
     async def random_categorized(self, category: str) -> Blague:
-        async with aiohttp.ClientSession(raise_for_status=True) as session:
-            
-            url = f"https://www.blagues-api.fr/api/type/{category}/random"
-            headers = {'Authorization': 'Bearer ' + self.token}
+        endpoint = f"/type/{category}/random"
+        data = await self._get(endpoint)
 
-            async with session.get(url, headers=headers) as resp:
-                data = await resp.json()
-                return Blague.parse_obj(data)
-    
+        return Blague.parse_obj(data)
+
     async def from_id(self, id: int) -> Blague:
-        async with aiohttp.ClientSession(raise_for_status=True) as session:
-            
-            url = f"https://www.blagues-api.fr/api/id/{id}"
-            headers = {'Authorization': 'Bearer ' + self.token}
-
-            async with session.get(url, headers=headers) as resp:
-                data = await resp.json()
-                return Blague.parse_obj(data)
+        
+        endpoint = f"/id/{id}"
+        data = await self._get(endpoint)
+        
+        return Blague.parse_obj(data)
 
     async def count(self) -> CountJoke:
-        async with aiohttp.ClientSession(raise_for_status=True) as session:
-            
-            url = "https://www.blagues-api.fr/api/count"
-            
-            headers = {'Authorization': 'Bearer ' + self.token}
-            
-            async with session.get(url, headers=headers) as resp:
-                data = await resp.json()
-                return CountJoke.parse_obj(data)
-    
+
+        endpoint = "/count"
+        data = await self._get(endpoint)
+        
+        return CountJoke.parse_obj(data)
